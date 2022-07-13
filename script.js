7001346ab11e56afffbbf9ad146f0401918bedfc
Gabriel das Neves <galvesdasneves@gmail.com>
2022-07-12 22:56:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+  var conteudo = 'https://gabrieldasneves.github.io/Business-card/';
+  var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
+  var imagemQRCode = GoogleCharts + conteudo;
+  document.getElementById('imageQRCode').src = imagemQRCode;
